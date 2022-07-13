8032120142aeecf77bcbfc623bd4c85049c8bc09
Luis Vela <luismvl114@gmail.com>
2022-07-13 13:50:25 +0000

    function that changes the theme

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import React, { useState } from 'react'
 import { ThemeProvider } from 'styled-components'
 import { dark, light, base } from '../../styles/themes'
@@ -8,10 +7,18 @@ import Navbar from '../Navbar'
 function App() {
   const [selectedTheme, setSelectedTheme] = useState(light)
 
+  const toggleTheme = () => {
+    if (selectedTheme.name === 'light') setSelectedTheme(dark)
+    else setSelectedTheme(light)
+  }
+
   return (
     <ThemeProvider theme={{ ...selectedTheme, ...base }}>
       <Navbar />
       <h1>Test</h1>
+      <button type="button" onClick={toggleTheme}>
+        Change theme
+      </button>
     </ThemeProvider>
   )
 }
